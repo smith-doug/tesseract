0d699935ab742a12c9c5fc0c6c0263ee2798b164
Reid Christopher <reid.christopher@swri.org>
2023-09-07 23:45:50 +0000

    Fixes for 22.04/Humble

--- tesseract_scene_graph/src/graph.cpp
+++ tesseract_scene_graph/src/graph.cpp
@@ -36,6 +36,9 @@ TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/base_object.hpp>
 #include <boost/serialization/nvp.hpp>
+#if (BOOST_VERSION >= 107400) && (BOOST_VERSION < 107500)
+#include <boost/serialization/library_version_type.hpp>
+#endif
 #include <boost/serialization/shared_ptr.hpp>
 #include <boost/serialization/split_member.hpp>
 #include <boost/serialization/unordered_map.hpp>
